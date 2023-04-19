4428c8e1c9e4e3ca00e0d7a0053dcea1f0faf5f8
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-19 08:43:05 +0530

    Calculating minimum string value amoung three string string values

--- GenericsPracticeProblems/Program.cs
+++ GenericsPracticeProblems/Program.cs
@@ -11,7 +11,7 @@ namespace GenericsPracticeProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics Practice Problems");
-            Console.WriteLine("1.Minimum Integer\n2.Minimum FloatValue\n");
+            Console.WriteLine("1.Minimum Integer\n2.Minimum Float Value\n3.Mimum string Value");
             Console.WriteLine("Choose your Option\n");
             int Option =Convert.ToInt32(Console.ReadLine());
 
@@ -22,6 +22,9 @@ namespace GenericsPracticeProblems
                     break;
                 case 2:
                     MinimumFloatValue.GivenFloatValues(10.2f, 20.3f, 40.2f);
+                    break;
+                case 3:
+                    MinimumStringValue.GivenStringValues("Komma", "Siva", "Jyothi");
                     break;
             }
             Console.ReadLine();
