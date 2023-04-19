6320f40041bdfae18a377c7a36ac72e55b3bc9cc
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-19 08:15:34 +0530

    Displaying Welcome message of Generics practice problems

---
+++ GenericsPracticeProblems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Practice Problems");
+            Console.ReadLine();
+        }
+    }
+}
