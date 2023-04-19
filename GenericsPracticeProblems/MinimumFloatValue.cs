ae03c3d13ac14a32e4a6d3735bd1f0dedfd0b20d
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-19 08:37:09 +0530

    Calculating Float value amoung three numbers

---
+++ GenericsPracticeProblems/MinimumFloatValue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPracticeProblems
+{
+    internal class MinimumFloatValue
+    {
+        public static float GivenFloatValues(float Firstvalue, float Secondvalue, float Thirdvalue)
+        {
+            if (Firstvalue.CompareTo(Secondvalue) < 0 && Firstvalue.CompareTo(Thirdvalue) < 0 ||
+               Firstvalue.CompareTo(Secondvalue) <= 0 && Firstvalue.CompareTo(Thirdvalue) < 0 ||
+               Firstvalue.CompareTo(Secondvalue) < 0 && Firstvalue.CompareTo(Thirdvalue) <= 0)
+            {
+                Console.WriteLine($"First vale {Firstvalue} is Minimum than Second value {Secondvalue} and Third value {Thirdvalue}");
+                return Firstvalue;
+            }
+            if (Secondvalue.CompareTo(Firstvalue) < 0 && Secondvalue.CompareTo(Thirdvalue) < 0 ||
+                Secondvalue.CompareTo(Firstvalue) <= 0 && Secondvalue.CompareTo(Thirdvalue) < 0 ||
+                Secondvalue.CompareTo(Firstvalue) < 0 && Secondvalue.CompareTo(Thirdvalue) <= 0)
+            {
+                Console.WriteLine($"Second vale {Secondvalue} is Minimum than First value {Firstvalue} and Third value {Thirdvalue}");
+                return Secondvalue;
+            }
+            if (Thirdvalue.CompareTo(Firstvalue) < 0 && Thirdvalue.CompareTo(Secondvalue) < 0 ||
+               Thirdvalue.CompareTo(Firstvalue) <= 0 && Thirdvalue.CompareTo(Secondvalue) < 0 ||
+               Thirdvalue.CompareTo(Firstvalue) < 0 && Thirdvalue.CompareTo(Secondvalue) <= 0)
+            {
+                Console.WriteLine($"Third value {Thirdvalue} is Minimum than  First vale {Firstvalue} and Second value {Secondvalue}");
+                return Thirdvalue;
+            }
+            return Firstvalue;
+        }
+    }
+}
--- GenericsPracticeProblems/Program.cs
+++ GenericsPracticeProblems/Program.cs
@@ -11,14 +11,17 @@ namespace GenericsPracticeProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics Practice Problems");
-            Console.WriteLine("1.Minimum Integer\n");
-            Console.WriteLine("Chhose your Option\n");
+            Console.WriteLine("1.Minimum Integer\n2.Minimum FloatValue\n");
+            Console.WriteLine("Choose your Option\n");
             int Option =Convert.ToInt32(Console.ReadLine());
 
             switch(Option)
             {
                 case 1:
                     MinInteger.GivenIntegers(100, 201, 30);
+                    break;
+                case 2:
+                    MinimumFloatValue.GivenFloatValues(10.2f, 20.3f, 40.2f);
                     break;
             }
             Console.ReadLine();
